9f8376f35dab2309c79693882469181965aa438a
ReddikHaien <584969@stud.hvl.no>
2022-05-09 17:29:31 +0200

    Added Outwards corners to ramps

--- Assets/Scripts/World/TileData/Models/RampModel.cs
+++ Assets/Scripts/World/TileData/Models/RampModel.cs
@@ -16,6 +16,8 @@ namespace ModelImplementation{
         public RampModel(){
             var models = ModelLoader.LoadModel("Models/Ramp");
             edge = models.AsEnumerable().First().Value;
+            var cornerOutModels = ModelLoader.LoadModel("Models/RampCornerOutwards");
+            cornerOut = cornerOutModels.AsEnumerable().First().Value;
         }
 
         public override string Name => "Ramp";
@@ -24,27 +26,70 @@ namespace ModelImplementation{
         {
             var wp = new Vector3Int(chunkPosition.x*Chunk.WIDTH,chunkPosition.y,chunkPosition.z*Chunk.WIDTH) + tilePosition;
             var tp = new Vector3Int(Chunk.TILE_WIDTH,Chunk.TILE_HEIGHT,Chunk.TILE_WIDTH) * tilePosition;
-            var pz = world.IsSideVisible(wp + Vector3Int.forward,AbstractShape.Direction.Back);
-            var nz = world.IsSideVisible(wp + Vector3Int.back,AbstractShape.Direction.Front);
-            var px = world.IsSideVisible(wp + Vector3Int.right,AbstractShape.Direction.Left);
-            var nx = world.IsSideVisible(wp + Vector3Int.left, AbstractShape.Direction.Right);
+            
+            var nz = isSideTall(getShapeAt(remote,world,wp + Vector3Int.back));
+            var pz = isSideTall(getShapeAt(remote,world,wp + Vector3Int.forward));
+            var nx = isSideTall(getShapeAt(remote,world,wp + Vector3Int.left));
+            var px = isSideTall(getShapeAt(remote,world,wp + Vector3Int.right));
+            
+            var nznx = isSideTall(getShapeAt(remote,world,wp + Vector3Int.back + Vector3Int.left));
+            var nzpx = isSideTall(getShapeAt(remote,world,wp + Vector3Int.back + Vector3Int.right));
+            var pznx = isSideTall(getShapeAt(remote,world,wp + Vector3Int.forward + Vector3Int.left));
+            var pzpx = isSideTall(getShapeAt(remote,world,wp + Vector3Int.forward + Vector3Int.right));
 
             var matdef = remote.GetMaterialDefinition(tile.BaseMaterial);
             var material = materialManager.GetMaterial2(matdef).DefaultTexture;
 
-            if (!nz && pz && px && nx){
+            if (nz && !pz && !px && !nx){
                 builder.AddModel(edge,tp,material);
             }
-            else if (!pz && nz && px && nx){
+            else if (pz && !nz && !px && !nx){
                 builder.AddModel(edge,tp,material,yRotation: 180.0f);
             }
-            else if (!px && nz && pz && nx){
+            else if (px && !nz && !pz && !nx){
                 builder.AddModel(edge,tp,material,yRotation: -90.0f);
             }
-            else if (!nx && nz && pz && px){
+            else if (nx && !nz && !pz && !px){
                 builder.AddModel(edge,tp,material,yRotation: 90.0f);
             }
+            else{
+                //00#
+                //0|0
+                //000
+                if (pzpx && !nzpx && !pznx && !nznx && !px && !pz){
+                    builder.AddModel(cornerOut,tp,material, yRotation: 180.0f);
+                }
+                //#00
+                //0|0
+                //000
+                else if (nzpx && !pzpx && !nznx && !pznx && !px && !nz){
+                    builder.AddModel(cornerOut,tp,material, yRotation: -90.0f);    
+                }
+                //000
+                //0|0
+                //#00
+                else if (nznx && !pzpx && !pznx && !nzpx && !nx && !nz){
+                    builder.AddModel(cornerOut,tp,material);    
+                }
+                //000
+                //0|0
+                //00#
+                else if (pznx && !pzpx && !nznx && !nzpx && !nx && !pz){
+                    builder.AddModel(cornerOut,tp,material, yRotation: 90.0f);    
+                }
+            }
         }
+
+        private RemoteFortressReader.TiletypeShape getShapeAt(RemoteManager remote, World world, Vector3Int wp){
+            var tile = world.GetTile(wp.x,wp.y,wp.z);
+            return remote.GetTiletype(tile.TileType).Shape;
+        }
+
+        private bool isSideTall(RemoteFortressReader.TiletypeShape shape) => shape switch
+        {
+            RemoteFortressReader.TiletypeShape.Wall => true,
+            _ => false
+        };
     }
 }
 
